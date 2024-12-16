357ddaac6e43a54f96054e44121365e61ff9d688
restonic4 <marcosruizlerma@gmail.com>
2024-12-17 00:09:21 +0100

    Book gui textures

---
+++ src/main/java/com/restonic4/forgotten/mixin/client/LecternRenderMixin.java
@@ -0,0 +1,54 @@
+package com.restonic4.forgotten.mixin.client;
+
+import com.mojang.blaze3d.vertex.PoseStack;
+import com.mojang.blaze3d.vertex.VertexConsumer;
+import com.mojang.math.Axis;
+import com.restonic4.forgotten.Forgotten;
+import com.restonic4.forgotten.registries.common.ForgottenItems;
+import net.minecraft.client.gui.GuiGraphics;
+import net.minecraft.client.model.BookModel;
+import net.minecraft.client.renderer.MultiBufferSource;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.blockentity.EnchantTableRenderer;
+import net.minecraft.client.renderer.blockentity.LecternRenderer;
+import net.minecraft.client.renderer.texture.TextureAtlas;
+import net.minecraft.client.resources.model.Material;
+import net.minecraft.core.Direction;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.level.block.LecternBlock;
+import net.minecraft.world.level.block.entity.LecternBlockEntity;
+import net.minecraft.world.level.block.state.BlockState;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(LecternRenderer.class)
+public class LecternRenderMixin {
+    @Shadow @Final private BookModel bookModel;
+    private static final Material ETHEREAL_BOOK_LOCATION = new Material(TextureAtlas.LOCATION_BLOCKS, new ResourceLocation(Forgotten.MOD_ID, "entity/ethereal_book"));
+
+    @Inject(method = "render(Lnet/minecraft/world/level/block/entity/LecternBlockEntity;FLcom/mojang/blaze3d/vertex/PoseStack;Lnet/minecraft/client/renderer/MultiBufferSource;II)V", at = @At("HEAD"), cancellable = true)
+    public void render(LecternBlockEntity lecternBlockEntity, float f, PoseStack poseStack, MultiBufferSource multiBufferSource, int i, int j, CallbackInfo ci) {
+        BlockState blockState = lecternBlockEntity.getBlockState();
+        System.out.println(blockState.getValue(LecternBlock.HAS_BOOK) + " and " + lecternBlockEntity.getBook().is(ForgottenItems.ETHEREAL_WRITTEN_BOOK));
+        System.out.println(lecternBlockEntity.getBook());
+        if ((Boolean)blockState.getValue(LecternBlock.HAS_BOOK) && lecternBlockEntity.getBook().is(ForgottenItems.ETHEREAL_WRITTEN_BOOK)) {
+            poseStack.pushPose();
+            poseStack.translate(0.5F, 1.0625F, 0.5F);
+            float g = ((Direction)blockState.getValue(LecternBlock.FACING)).getClockWise().toYRot();
+            poseStack.mulPose(Axis.YP.rotationDegrees(-g));
+            poseStack.mulPose(Axis.ZP.rotationDegrees(67.5F));
+            poseStack.translate(0.0F, -0.125F, 0.0F);
+            this.bookModel.setupAnim(0.0F, 0.1F, 0.9F, 1.2F);
+            VertexConsumer vertexConsumer = ETHEREAL_BOOK_LOCATION.buffer(multiBufferSource, RenderType::entitySolid);
+            this.bookModel.render(poseStack, vertexConsumer, i, j, 1.0F, 1.0F, 1.0F, 1.0F);
+            poseStack.popPose();
+
+            ci.cancel();
+        }
+    }
+}
